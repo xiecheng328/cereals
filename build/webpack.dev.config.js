8afd1438ea4d71b5d2281aaf7e5b28d80dc47af7
wuying <wuy_123@126.com>
2018-04-20 12:59:23 +0800

    aaa

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -15,7 +15,7 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 module.exports = merge(webpackBaseConfig, {
     devtool: '#source-map',
     output: {
-        publicPath: './dist/',
+        publicPath: '/dist/',
         filename: '[name].js',
         chunkFilename: '[name].chunk.js'
     },
